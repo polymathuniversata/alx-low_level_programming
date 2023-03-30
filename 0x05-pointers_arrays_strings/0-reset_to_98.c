a904c3fb40076893cc7e008da09e45eb293a463e
polymathuniversata <polymthuniversata@outlook.com>
2023-03-30 02:45:04 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* reset_to_98 - updates the value of an integer to 98
-* @n: pointer to an integer
-*/
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
+ * @n: input integer.
+ * Return: no return.
+ */
 void reset_to_98(int *n)
 {
-	if (_isupper(*n))
-		*n = 98;
+	*n = 98;
 }
