00adf4761e56cd5a7c719b7964f7ab56e08c8896
polymathuniversata <polymthuniversata@outlook.com>
2023-03-30 18:27:25 -0700

    works fine

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,18 +7,20 @@
  */
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+    int r = 0, c = 0;
+    time_t t;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+    srand((unsigned int) time(&t));
+    while (c < 2772)
+    {
+        r = rand() % 128;
+        if ((c + r) > 2772)
+            break;
+        c = c + r;
+        printf("%c", r);
+    }
+    printf("%c\n", (2772 - c));
+    if (system("echo Tada! Congrats | cat -e 1>&2 | tr -d '\\n\\0'") == -1)
+        return (1);
+    return (0);
 }
