4c9b7564d4df375bb2d17ccc6e1851a964795827
polymathuniversata <polymthuniversata@outlook.com>
2023-03-30 18:23:36 -0700

    works fine

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	int r = 0, c = 0;
+	time_t t;
 
-#define PASSWORD_LENGTH 10
-
-int main(void) {
-    srand(time(NULL));  // seed the random number generator with current time
-
-    char password[PASSWORD_LENGTH+1];  // create a char array to hold the password
-
-    // fill the password array with random characters
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = rand() % 94 + 33;  // generate a random ASCII character from 33 to 126
-    }
-
-    password[PASSWORD_LENGTH] = '\0';  // null-terminate the password string
-
-    printf("Random password for program 101-crackme: %s\n", password);
-
-    return 0;
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
 }
