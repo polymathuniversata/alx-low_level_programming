40803255c931d2b42c0451f97d87139e6b34c88c
polymathuniversata <polymthuniversata@outlook.com>
2023-04-10 18:48:42 -0700

    Errors Fixed

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,13 @@
-#!/bin/bash
 #include "main.h"
 
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+		"\t- Kevin Mitnick");
 	return (0);
 }
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,5 @@
-#!/bin/bash
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,6 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 
 #endif
